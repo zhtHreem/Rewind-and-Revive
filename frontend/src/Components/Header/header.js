43f0e78f22a483f9a2529828b140ffd64baeca03
Hareem <zainabhareemtayyab@gmail.com>
2024-09-10 20:46:21 +0500

    added swiper

--- frontend/src/Components/Header/header.js
+++ frontend/src/Components/Header/header.js
@@ -1,38 +1,29 @@
 import React from 'react';
-import { Box, Button, List, ListItem, ListItemIcon, ListItemText, IconButton,CardMedia, Stack, Grid, Typography, Card } from '@mui/material';
+import { Box, Button, List, ListItem, ListItemIcon,AppBar, Toolbar,  ListItemText, IconButton,CardMedia, Stack, Grid, Typography, Card } from '@mui/material';
+import { useMediaQuery } from '@mui/material';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
 import AddSharpIcon from '@mui/icons-material/AddSharp';
 import { Link } from 'react-router-dom';
 import Navbar from '../Navbar/navbar';
+import MenuIcon from '@mui/icons-material/Menu';
+import 'swiper/css';
+import { Navigation,Pagination } from 'swiper/modules'; // Import Swiper modules
+import Swipe from './Swiper/pagination';
 
-function DiagonalBox() {
+
+function Header() {
+  
   return (
     <>
     <Navbar/>
     <Box p={{ xs: 1, md: 8 }} sx={{ position: 'relative' }}>
       <Grid container spacing={{ xs: 3, md: 8 }}>
         <Grid item xs={12} md={5}>
-          <Typography
-            variant='h2'
-            fontFamily={"'Times New Roman', serif"}
-            fontSize={{
-              xs: '2rem',
-              sm: '2.5rem',
-              md: '3rem',
-              lg: '3.5rem',
-            }}
-          >
+          <Typography variant='h2' fontFamily={"'Times New Roman', serif"}fontSize={{xs: '2rem', sm: '2.5rem',   md: '3rem',lg: '3.5rem',  }}>
             Rewind and Revive
           </Typography>
-          <Typography
-            variant='body2'
-            fontSize={{
-              xs: '0.75rem',
-              sm: '0.875rem',
-              md: '1rem',
-              lg: '1.125rem',
-            }}
-          >
+          <Typography variant='body2'  fontSize={{  xs: '0.75rem', sm: '0.875rem', md: '1rem', lg: '1.125rem', }} >
             Discover sustainable fashion at Rewind & Revive, the ultimate online thrift store for unique, second-hand treasures
           </Typography>
           <Stack direction="row" py={2}>
@@ -43,37 +34,18 @@ function DiagonalBox() {
         </Grid>
 
         {/* Nested Grid Container */}
-        <Grid container spacing={10} marginTop={-9} item xs={12} md={7} >
+        <Grid container spacing={10}  item xs={12} md={7}>
           <Grid item xs={6} md={6}  >
 
-          <Box
-      
-      sx={{
-        width: '100%',
-        minHeight: {lg:'81%',xl:'100%'}, // Ensure the Box can grow with content
-        
-        background: 'linear-gradient(45deg, #ff5733, #ffd633)',
-        border: '2px solid #fffefa',
-        transform: 'rotate(5deg)',
-        display: 'flex',
-        alignItems: 'center', // Center items vertically if needed
-        justifyContent: 'center', // Center items horizontally if needed
-        position:"relative"
-      }}
-    >
-      <Card>
-        <CardMedia
-          component="img"
-          height="auto" // Allow height to be auto-adjusted
-          image={require("./images/pink.jpg")}
-          alt="Description"
-          sx={{position:"absolute",transform: 'rotate(355deg)',top:5,left:-5}}
-        />
-      </Card>
-    </Box>
+             <Box marginLeft={3} sx={{ minWidth:100,width: {xs: '100%',sm:300}, height:{xs:400,md:470}, background: 'linear-gradient(45deg, #ff5733, #ffd633)',   border: '2px solid #fffefa', transform: 'rotate(5deg)', display: 'flex',   alignItems: 'center',   justifyContent: 'center', position:"relative" }} >
+               <Card>
+                 <CardMedia component="img" image={require("./images/pink.jpg")}  alt="Description"  sx={{width:"100%",height:"100%",position:"absolute",transform: 'rotate(355deg)',top:5,left:-5}} />
+                </Card>
+             </Box>
           </Grid>
 
-          <Grid item xs={6} md={6} justifyContent={"space-between"}  >
+          <Grid item xs={6} md={6} justifyContent={"space-between"} p={3} >
+
             <Stack direction="row" spacing={2}>
               <IconButton  sx={{ border: '2px solid #000', borderRadius: '1' }}>
                 <AddSharpIcon  sx={{ color: "orangered",fontSize:{ xs: "0.75rem" } }} />
@@ -83,44 +55,19 @@ function DiagonalBox() {
                 Explore our curated collection of pre-loved treasures
               </Typography>
             </Stack>
-            <Box
-              marginTop={4}
-              p={2}
-              sx={{
-                width: 'auto',
-                
-                background: 'linear-gradient(135deg, #ff5733 50%, #ffd633 50%)',
-                border: '2px solid #fffefa',
-              }}
-            >
-                
-        <CardMedia
-          component="img"
-          sx={{ height: '425px', width: 'auto' }}
-          // Allow height to be auto-adjusted
-          image={require("./images/orange.jpg")}
-          alt="Description"
-        
-        />
-      
+
+
+            <Box  marginTop={4} p={{xs:1,sm:2}}sx={{ minWidth:100,width: {xs: '100%',sm:250},height: {xs:300,sm:400},  background: 'linear-gradient(135deg, #FAF9F6 50%,  #ffd633 50%)',  border: '2px solid #fffefa', }}>
+                  <CardMedia component="img"sx={{ height: '100%', width: '100%' }}image={require("./images/orange.jpg")} alt="Description" />
             </Box>
           </Grid>
         </Grid>
 
         {/* "What Makes Us Pro?" Section Moves Below on Small Screens */}
-        <Grid item xs={6} md={12} sx={{ position: {xs:"none",md:'absolute'},zIndex: 10,bottom:30 }}>
+        <Grid item xs={12} my={{md:15}} sx={{ position: {xs:"none",md:'absolute'},zIndex: 10,bottom:70 }}>
           <Stack py={2}>
-            <Typography
-              variant="h5"
-              fontFamily={"'Times New Roman', serif"}
-              fontSize={{
-                xs: '1.5rem',
-                sm: '1.75rem',
-                md: '2rem',
-                lg: '2.25rem',
-              }}
-            >
-              What Makes Us Pro?
+            <Typography variant="h5" fontFamily={"'Times New Roman', serif"} fontSize={{  xs: '1.5rem', sm: '1.75rem',md: '2rem',  lg: '2.25rem',}} >
+                 What Makes Us Pro?
             </Typography>
             <List>
               <ListItem>
@@ -143,9 +90,13 @@ function DiagonalBox() {
           </Stack>
         </Grid>
       </Grid>
+      
+      <Grid item xs={12}>
+         <Swipe/>
+         </Grid>
     </Box>
     </>
   );
 }
 
-export default DiagonalBox;
+export default Header;

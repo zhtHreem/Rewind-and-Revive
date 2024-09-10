eaa9137fb434239538906a783948be910385a0ea
Hareem <zainabhareemtayyab@gmail.com>
2024-09-11 02:28:19 +0500

    changed background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/header';
+import Bidding from './Components/Bidding/Bidding';
+import Layout from './Components/Layout/layout';
 function App() {
   return (
     <div className="App">
+        <Layout>
         <Header/>
+        <Bidding/>
+        </Layout>
     </div>
   );
 }

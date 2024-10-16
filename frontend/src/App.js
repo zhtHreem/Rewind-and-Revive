f2ee8a6894d779e39de1c6273a03468bab42e93e
Hareem <zainabhareemtayyab@gmail.com>
2024-10-17 03:02:41 +0500

    added more components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 import Header from './Components/Header/header';
 import Bidding from './Components/Bidding/Bidding';
 import Layout from './Components/Layout/layout';
+import LimitedTimeDeals from './Components/LimitedTimeDeals/limitedtimedeals';
+import PopularFabricsSection from './Components/MostPopularItem/mostPopular';
 function App() {
   return (
     <div className="App">
         <Layout>
         <Header/>
+        <LimitedTimeDeals/>
+        <PopularFabricsSection/>
         <Bidding/>
         </Layout>
     </div>

2eb00a6521d558befd5af28b956ba459c04b7f79
Hareem <zainabhareemtayyab@gmail.com>
2024-12-08 02:20:49 +0500

    configured db

---
+++ backend/src/config/db.js
@@ -0,0 +1,17 @@
+
+import mongoose from 'mongoose';
+
+const connectDB = async ()=>{
+ try{                                        
+    const mongoURI=process.env.MONGO_URI ||'mongodb+srv://admin:123@eventify.dkeujvr.mongodb.net/RewindAndRevive?retryWrites=true&w=majority&appName=eventify';
+    
+    await mongoose.connect(mongoURI);
+    console.log('MongoDB connected...');
+} catch (error) {
+     console.error('Error connecting to MongoDB:', error.message);
+     process.exit(1); 
+}
+};
+export default connectDB; 
+
+

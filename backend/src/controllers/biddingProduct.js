07cdd4e44d55fcd229d7aca6ebeb3e917a199773
afeefaKhaliq <afeefakhaliq045@gmail.com>
2024-12-08 20:06:43 +0500

    controller

--- backend/src/controllers/biddingProduct.js
+++ backend/src/controllers/biddingProduct.js
@@ -48,7 +48,6 @@ export const createProduct = async (req, res) => {
         description,
         bidStartTime,
         bidEndTime,
-        biddingDay,
         imageUrl: `/uploads/${req.file.filename}`, // Store file path
       });
 

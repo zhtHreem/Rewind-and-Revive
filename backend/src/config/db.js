c5be74729aa529882a0863c7b293072851285b7c
Hareem <zainabhareemtayyab@gmail.com>
2025-01-10 23:07:05 +0500

    configured env

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,6 +1,7 @@
 
 import mongoose from 'mongoose';
-
+import dotenv from 'dotenv';
+dotenv.config();
 const connectDB = async ()=>{
  try{                                        
     const mongoURI=process.env.MONGO_URI ;

d3504e04ea58ce2bd051ef8c2b3bb4dc069d97cb
afeefaKhaliq <afeefakhaliq045@gmail.com>
2025-05-06 03:42:08 +0500

    stripe

--- backend/src/models/payment.js
+++ backend/src/models/payment.js
@@ -1,16 +1,13 @@
 import mongoose from 'mongoose';
 
 const PaymentSchema = new mongoose.Schema({
-    productBuyers: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    productOwner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true }, 
+  productBuyers: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  productOwner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true }, 
+  amount: { type: Number, required: true },
+  currency: { type: String, default: 'pkr' },
+  createdAt: { type: Date, default: Date.now }
+});
 
-    cardNumber: { type: String, required: true }, 
-    cvv: { type: Number, required: true }, 
-    name: { type: String, required: true},
-    expirationDate: { type: String, required: true},
-   createdAt: { type: Date, default: Date.now }
-}); 
-const Payment= mongoose.model('payment', PaymentSchema);
-
+const Payment = mongoose.model('Payment', PaymentSchema);
 export default Payment;
